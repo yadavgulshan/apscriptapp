53385b76ead9cabc6773f546c701d3f1d140c38b
yadavgulshan <yadavgulshan542@gmail.com>
2021-02-07 12:03:05 +0530

    completed using pdf plugin

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyHomePage extends StatelessWidget {
       margin: pw.EdgeInsets.all(32),
       build: (pw.Context context) {
         return <pw.Widget>[
-          pw.Header(level: 0, child: pw.Text("Easy Approach Document")),
+          pw.Header(level: 0, child: pw.Text("My Doc")),
           pw.Paragraph(
               text:
                   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas sed tempus urna. Quisque sagittis purus sit amet. A arcu cursus vitae congue mauris rhoncus aenean vel elit. Ipsum dolor sit amet consectetur adipiscing elit pellentesque. Viverra justo nec ultrices dui sapien eget mi proin sed."),
