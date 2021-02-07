b1397d428cd0c6e2b9d11a7fd4a5ad8b13b09a8c
yadavgulshan <yadavgulshan542@gmail.com>
2021-02-07 10:03:10 +0530

    trying pdf stuff

---
+++ lib/pages/resume.dart
@@ -0,0 +1,19 @@
+// import 'package:pdf/pdf.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/widgets.dart' as pw;
+
+// class Resume extends StatefulWidget {
+//   Resume({Key key}) : super(key: key);
+
+//   @override
+//   _ResumeState createState() => _ResumeState();
+// }
+
+// class _ResumeState extends State<Resume> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//        child: child,
+//     );
+//   }
+// }
